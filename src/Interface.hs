de44159ba32fe8f92d56faaaa529b479188e5f69
pedro-paulo-marquez <pedro.marquez@ufu.br>
2025-09-18 09:53:04 -0300

    Update Interface.hs

--- src/Interface.hs
+++ src/Interface.hs
@@ -5,6 +5,7 @@ import Algorithms
 import Order
 import Validation
 import Leitura
+import FuncoesImpuras
 import Data.Time (Year)
 import System.IO (stdout, hSetBuffering, BufferMode (NoBuffering))
 
@@ -19,16 +20,20 @@ prompt msg = do putStr msg
 
 interface :: IO ()
 interface = do hSetBuffering stdout NoBuffering
-               conteudo <- readFile "itens.csv"
-               let x = lines conteudo
+               conteudoIt <- readFile "itens.csv"
+               let x = lines conteudoIt
                length x `seq` return ()
                let y = filtrarItensValidos (lerVariosItens x)
-               menuPrincipal y
+               conteudoUs <- readFile "usuarios.csv"
+               let w = lines conteudoUs
+               length w `seq` return ()
+               let z = filtrarUsuariosValidos (lerVariosUsuarios w)
+               menuPrincipal y z
 
 
 -- MENU PRINCIPAL --
-menuPrincipal :: [Item] -> IO ()
-menuPrincipal listaIt = do
+menuPrincipal :: [Item] -> [Usuario] -> IO ()
+menuPrincipal listaIt listaUs = do
     linha
     putStrLn "  Sistema de Mı́dias - Menu Principal"
     linha
@@ -45,8 +50,8 @@ menuPrincipal listaIt = do
     
     opcao <- getLine
     case opcao of
-        "1" -> menuCadastroItens listaIt
-        "2" -> menuCadastroUsuarios listaIt
+        "1" -> menuCadastroItens listaIt listaUs
+        "2" -> menuCadastroUsuarios listaIt listaUs
         "3" -> menuEmpDev listaIt
         "4" -> menuBuscaListagem listaIt
         "5" -> menuRelatEstat listaIt
@@ -56,11 +61,11 @@ menuPrincipal listaIt = do
     -- Nao implementei o 0 ainda
         _   -> do
             putStrLn "\nOpção inválida!\n"
-            menuPrincipal listaIt 
+            menuPrincipal listaIt listaUs
 
 -- SUBMENU: Cadastro de Itens --
-menuCadastroItens :: [Item] -> IO ()
-menuCadastroItens listaIt = do
+menuCadastroItens :: [Item] -> [Usuario] -> IO ()
+menuCadastroItens listaIt listaUs = do
     linha
     putStrLn "\tCadastro de Itens"
     linha
@@ -73,19 +78,41 @@ menuCadastroItens listaIt = do
     opcao <- getLine
     case opcao of
         "1" -> do it <- lendoItem
-                  inserirItem "itens.csv" it listaIt
-                  putStr (formatarItem it)
-     --   "2" -> 
-     --   "3" -> 
-        "0" -> menuPrincipal listaIt
+                  if (validarItem it) == Valido &&
+                     not (codigoJaExiste (codigo it) listaIt) &&
+                     not (itemJaExiste it listaIt)
+                   then do inserirItem "itens.csv" it listaIt
+                           let novaListaIt = colocarNaLista it listaIt
+                           putStr ("\n" ++ (formatarItem it))
+                           menuPrincipal novaListaIt listaUs
+                   else do putStrLn "Ocorreu algo de errado com a inserção."
+                           menuCadastroItens listaIt listaUs
+
+        "2" -> do putStr (variosPorCod listaIt)
+                  cod <- getLine
+                  if (codigoJaExiste cod listaIt)
+                   then do let tirado = head (buscarItCod cod listaIt)
+                           let novaListaIt = tirarDaLista tirado listaIt
+                           listaItParaArq "itens.csv" novaListaIt
+                           putStr ("\n" ++ (show novaListaIt))
+                           menuPrincipal novaListaIt listaUs
+                   else do putStrLn "Ocorreu algo de errado com a remoção."
+                           menuCadastroItens listaIt listaUs
+                  
+        "3" -> do putStrLn (variosPorCod listaIt)
+                  putStr "Pressione qualquer Enter para continuar. "
+                  tecla <- getLine
+                  case tecla of
+                    _ -> menuPrincipal listaIt listaUs
+        "0" -> menuPrincipal listaIt listaUs
         _   -> do
             putStrLn "\nOpção inválida!\n"
-            menuCadastroItens listaIt           
+            menuCadastroItens listaIt listaUs          
             
 
 -- SUBMENU: Cadastro de Usuários --
-menuCadastroUsuarios :: [Item] -> IO ()
-menuCadastroUsuarios listaIt = do
+menuCadastroUsuarios :: [Item] -> [Usuario] -> IO ()
+menuCadastroUsuarios listaIt listaUs = do
     linha
     putStrLn "\tCadastro de Usuarios"
     linha
@@ -94,17 +121,38 @@ menuCadastroUsuarios listaIt = do
     putStrLn "3 - Listar usuários cadastrados"
     putStrLn "0 - Voltar ao menu principal"
     putStr "Digite uma opção: "
-{-
+
     opcao <- getLine
     case opcao of
-        "1" -> 
-        "2" -> 
-        "3" -> 
-        "0" -> menuPrincipal listaIt
+        "1" -> do us <- lendoUsuario
+                  if (validarUsuario us) == Valido &&
+                     not (matriculaJaExiste (matricula us) listaUs) &&
+                     not (usuarioJaExiste us listaUs)
+                   then do inserirUsuario "usuarios.csv" us listaUs
+                           let novaListaUs = colocarNaLista us listaUs
+                           putStr ("\n" ++ (formatarUsuario us))
+                           menuPrincipal listaIt listaUs
+                   else do putStrLn "Ocorreu algo de errado com a inserção."
+                           menuCadastroItens listaIt listaUs
+
+        "2" -> do putStr (variosPorMat listaUs)
+                  mat <- getLine
+                  if (matriculaJaExiste mat listaUs)
+                   then do let tirado = head (buscarUsMat mat listaUs)
+                           let novaListaUs = tirarDaLista tirado listaUs
+                           listaUsParaArq "usuarios.csv" novaListaUs
+                           putStr ("\n" ++ (show novaListaUs))
+                           menuPrincipal listaIt novaListaUs
+                   else do putStrLn "Ocorreu algo de errado com a remoção."
+                           menuCadastroItens listaIt listaUs
+        "3" -> do putStrLn (variosPorMat listaUs)
+                  putStr "Pressione qualquer Enter para continuar. "
+                  tecla <- getLine
+                  case tecla of
+        "0" -> menuPrincipal listaIt listaUs
         _   -> do
             putStrLn "\nOpção inválida!\n"
-            menuCadastroUsuarios listaIt           -}
-
+            menuCadastroUsuarios listaIt listaUs
 
 
 -- SUBMENU: Empréstimos e Devoluções  --
@@ -128,7 +176,7 @@ menuEmpDev listaIt = do
         "3" -> 
         "4" ->
         "5" ->
-        "0" -> menuPrincipal listaIt
+        "0" -> menuPrincipal listaIt listaUs
         _   -> do
             putStrLn "\nOpção inválida!\n"
             menuEmpDev listaIt           -}
@@ -156,7 +204,7 @@ menuBuscaListagem listaIt = do
         "3" -> 
         "4" ->
         "5" ->
-        "0" -> menuPrincipal listaIt
+        "0" -> menuPrincipal listaIt listaUs
         _   -> do
             putStrLn "\nOpção inválida!\n"
             menuBuscaListagem listaIt           -}
@@ -186,7 +234,7 @@ menuRelatEstat listaIt = do
         "4" ->
         "5" ->
         "6" ->
-        "0" -> menuPrincipal listaIt
+        "0" -> menuPrincipal listaIt listaUs
         _   -> do
             putStrLn "\nOpção inválida!\n"
             menuRelatEstat listaIt           -}
@@ -208,7 +256,7 @@ menuEdicao listaIt = do
     case opcao of
         "1" -> 
         "2" -> 
-        "0" -> menuPrincipal listaIt
+        "0" -> menuPrincipal listaIt listaUs
         _   -> do
             putStrLn "\nOpção inválida!\n"
             menuEdicao listaIt           -}
@@ -230,7 +278,7 @@ menuExpImp listaIt = do
     case opcao of
         "1" -> 
         "2" -> 
-        "0" -> menuPrincipal listaIt
+        "0" -> menuPrincipal listaIt listaUs
         _   -> do
             putStrLn "\nOpção inválida!\n"
             menuExpImp listaIt           -}
@@ -251,78 +299,7 @@ menuAuditHist listaIt = do
     case opcao of
         "1" -> 
         "2" -> 
-        "0" -> menuPrincipal listaIt
+        "0" -> menuPrincipal listaIt listaUs
         _   -> do
             putStrLn "\nOpção inválida!\n"
             menuAuditHist listaIt           -}
-
-------------------------------------------------
-
--- FUNÇÕES QUE LIDAM COM ARQUIVOS --
-
-inserirItem :: FilePath -> Item -> [Item] -> IO ()
-inserirItem fp it listaIt = if (validarItem it) == Valido &&
-                               not (codigoJaExiste cod listaIt)
-                             then appendFile fp (formatarItem it)
-                             else appendFile fp ""
-    where cod = codigo it
-
-lendoItem :: IO Item
-lendoItem = do 
-               mid' <- getMidia
-               tit' <- getTitulo 
-               aut' <- getAutor mid' 
-               an0' <- getAno mid'
-               cod' <- getCodigo
-               let it = Item {titulo = tit', autor = aut', ano = an0', codigo = cod', midia = mid'}
-               return (it)
-             
-
-getMidia :: IO TipoMidia
-getMidia = do putStr "\nDigite o tipo de mídia: "
-              mid <- getLine
-              if validarMidia mid == Invalido
-                then do putStrLn "Campo Inválido! Tente novamente!"
-                        getMidia
-                else do let mid1 = read mid :: TipoMidia
-                        return (mid1)
-
-getTitulo :: IO String
-getTitulo = do putStr "Digite o titulo: "
-               tit <- getLine
-               if validarTitulo tit == Invalido
-                then do putStrLn "Campo Inválido! Tente novamente!"
-                        getTitulo
-                else return (tit)
-
-getAutor :: TipoMidia -> IO String
-getAutor m = do
-  case m of
-   Livro -> putStr "Digite o autor: "
-   Filme -> putStr "Digite o diretor: "
-   Jogo -> putStr "Digite o criador: "
-  aut <- getLine
-  if validarAutor aut == Invalido
-    then do putStrLn "Campo Inválido! Tente novamente!"
-            getAutor m
-    else return (aut)
-
-getAno :: TipoMidia -> IO Year
-getAno m = do putStr "Digite o ano: "
-              an0 <- getLine
-              if validarAnoString an0 == Invalido
-                then do putStrLn "Campo Inválido! Tente novamente!"
-                        getAno m
-                else do let an0year = read an0 :: Year
-                        if (validarAnoYear an0year m) == Invalido
-                         then do putStrLn "Campo Inválido! Tente novamente!"
-                                 getAno m
-                         else return (an0year)
-
-getCodigo :: IO String
-getCodigo = do putStr "Digite o codigo: "
-               cod <- getLine
-               if validCodigo cod == Invalido
-                 then do putStrLn "Campo Inválido! Tente novamente!"
-                         getCodigo
-                 else return (cod)
