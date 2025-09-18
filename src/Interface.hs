b4630d5f8fdfe3a47f39aa2ddff8c2c2ee4df466
pedro-paulo-marquez <pedro.marquez@ufu.br>
2025-09-17 23:16:14 -0300

    Update Interface.hs

--- src/Interface.hs
+++ src/Interface.hs
@@ -4,13 +4,27 @@ import Types
 import Algorithms
 import Order
 import Validation
-
-interface :: IO ()
-interface = do menuPrincipal []
+import Leitura
+import Data.Time (Year)
+import System.IO (stdout, hSetBuffering, BufferMode (NoBuffering))
 
 linha :: IO ()
 linha = do 
    putStrLn "========================================"
+
+prompt :: Read a => String -> IO a
+prompt msg = do putStr msg
+                readLn
+
+
+interface :: IO ()
+interface = do hSetBuffering stdout NoBuffering
+               conteudo <- readFile "itens.csv"
+               let x = lines conteudo
+               length x `seq` return ()
+               let y = filtrarItensValidos (lerVariosItens x)
+               menuPrincipal y
+
 
 -- MENU PRINCIPAL --
 menuPrincipal :: [Item] -> IO ()
@@ -55,16 +69,18 @@ menuCadastroItens listaIt = do
     putStrLn "3 - Listar itens cadastrados"
     putStrLn "0 - Voltar ao menu principal"
     putStr "Digite uma opção: "
-{-
+
     opcao <- getLine
     case opcao of
-        "1" -> 
-        "2" -> 
-        "3" -> 
+        "1" -> do it <- lendoItem
+                  inserirItem "itens.csv" it listaIt
+                  putStr (formatarItem it)
+     --   "2" -> 
+     --   "3" -> 
         "0" -> menuPrincipal listaIt
         _   -> do
             putStrLn "\nOpção inválida!\n"
-            menuCadastroItens listaIt           -}
+            menuCadastroItens listaIt           
             
 
 -- SUBMENU: Cadastro de Usuários --
@@ -241,3 +257,72 @@ menuAuditHist listaIt = do
             menuAuditHist listaIt           -}
 
 ------------------------------------------------
+
+-- FUNÇÕES QUE LIDAM COM ARQUIVOS --
+
+inserirItem :: FilePath -> Item -> [Item] -> IO ()
+inserirItem fp it listaIt = if (validarItem it) == Valido &&
+                               not (codigoJaExiste cod listaIt)
+                             then appendFile fp (formatarItem it)
+                             else appendFile fp ""
+    where cod = codigo it
+
+lendoItem :: IO Item
+lendoItem = do 
+               mid' <- getMidia
+               tit' <- getTitulo 
+               aut' <- getAutor mid' 
+               an0' <- getAno mid'
+               cod' <- getCodigo
+               let it = Item {titulo = tit', autor = aut', ano = an0', codigo = cod', midia = mid'}
+               return (it)
+             
+
+getMidia :: IO TipoMidia
+getMidia = do putStr "\nDigite o tipo de mídia: "
+              mid <- getLine
+              if validarMidia mid == Invalido
+                then do putStrLn "Campo Inválido! Tente novamente!"
+                        getMidia
+                else do let mid1 = read mid :: TipoMidia
+                        return (mid1)
+
+getTitulo :: IO String
+getTitulo = do putStr "Digite o titulo: "
+               tit <- getLine
+               if validarTitulo tit == Invalido
+                then do putStrLn "Campo Inválido! Tente novamente!"
+                        getTitulo
+                else return (tit)
+
+getAutor :: TipoMidia -> IO String
+getAutor m = do
+  case m of
+   Livro -> putStr "Digite o autor: "
+   Filme -> putStr "Digite o diretor: "
+   Jogo -> putStr "Digite o criador: "
+  aut <- getLine
+  if validarAutor aut == Invalido
+    then do putStrLn "Campo Inválido! Tente novamente!"
+            getAutor m
+    else return (aut)
+
+getAno :: TipoMidia -> IO Year
+getAno m = do putStr "Digite o ano: "
+              an0 <- getLine
+              if validarAnoString an0 == Invalido
+                then do putStrLn "Campo Inválido! Tente novamente!"
+                        getAno m
+                else do let an0year = read an0 :: Year
+                        if (validarAnoYear an0year m) == Invalido
+                         then do putStrLn "Campo Inválido! Tente novamente!"
+                                 getAno m
+                         else return (an0year)
+
+getCodigo :: IO String
+getCodigo = do putStr "Digite o codigo: "
+               cod <- getLine
+               if validCodigo cod == Invalido
+                 then do putStrLn "Campo Inválido! Tente novamente!"
+                         getCodigo
+                 else return (cod)
