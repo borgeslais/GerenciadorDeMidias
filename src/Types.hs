a41654071f7317f16976b6da83991f7794d67f9b
pedro-paulo-marquez <pedro.marquez@ufu.br>
2025-09-17 23:17:32 -0300

    Update Types.hs

--- src/Types.hs
+++ src/Types.hs
@@ -5,7 +5,7 @@ import Data.Time
 
 -- O Vazio serve para auxiliar na busca 
 data TipoMidia = Livro | Filme | Jogo | Nenhum
-  deriving (Show, Eq)
+  deriving (Show, Eq, Read)
 
 data Status = Emprestado | Disponivel | Nulo
   deriving (Show, Eq)
@@ -16,10 +16,9 @@ data Validade = Valido | Invalido
 data Item = Item
   { titulo :: String
   , autor :: String
-  , ano :: Year
+  , ano :: Year      -- Deve-se sempre colocar o ano da edição, não o de publicação da obra
   , codigo :: String
   , midia :: TipoMidia
-  , status :: Status
   } deriving (Eq)
  
  -- Não sei se devo incluir um [Item] em Usuario
@@ -32,28 +31,25 @@ data Usuario = Usuario
 -- INSTÂNCIAS --
 
 instance Show Item where
-  show (Item t au an c Livro s) = "Titulo: " ++ t ++
+  show (Item t au an c Livro) = "Titulo: " ++ t ++
                                 ", Autor: " ++ au ++
                                 ", Ano: " ++ show an ++
                                 ", Codigo: " ++ c ++
                                 ", Midia: " ++ show Livro ++
-                                ", Status: " ++ show s ++
                                 " |-| "
 
-  show (Item t au an c Filme s) = "Titulo: " ++ t ++
+  show (Item t au an c Filme) = "Titulo: " ++ t ++
                                 ", Diretor: " ++ au ++
                                 ", Ano: " ++ show an ++
                                 ", Codigo: " ++ c ++
                                 ", Midia: " ++ show Filme ++
-                                ", Status: " ++ show s ++
                                 " |-| "
 
-  show (Item t au an c Jogo s) = "Titulo: " ++ t ++
+  show (Item t au an c Jogo) = "Titulo: " ++ t ++
                                 ", Criador: " ++ au ++
                                 ", Ano: " ++ show an ++
                                 ", Codigo: " ++ c ++
                                 ", Midia: " ++ show Jogo ++
-                                ", Status: " ++ show s ++
                                 " |-| "
 
 instance Show Usuario where
@@ -63,17 +59,17 @@ instance Show Usuario where
                                 
 {- PARA TESTAR NO GHCI:
 
-generico = Item {titulo = "", autor = "", ano = -50000, codigo = "", midia = Nenhum, status = Nulo}
-x1 = Item {titulo = "Os Lusiadas", autor = "Camoes", ano = 1572, codigo = "101", midia = Livro, status = Emprestado}
-x2 = Item {titulo = "Rimas", autor = "Camoes", ano = 1595, codigo = "102", midia = Livro, status = Disponivel}
-x3 = Item {titulo = "Rimas", autor = "José Albano", ano = 1966, codigo = "103", midia = Livro, status = Emprestado}
-y1 = Item {titulo = "Festim Diabólico", autor = "Hitchcock", ano = 1948, codigo = "201", midia = Filme, status = Emprestado}
-y2 = Item {titulo = "Sangue de Heróis", autor = "John Ford", ano = 1948, codigo = "202", midia = Filme, status = Disponivel}
-y3 = Item {titulo = "Rastros de Ódio", autor = "John Ford", ano = 1956, codigo = "203", midia = Filme, status = Disponivel}
-z1 = Item {titulo = "Super Mario Bros.", autor = "Myamoto", ano = 1985, codigo = "301", midia = Jogo, status = Emprestado}
-z2 = Item {titulo = "Mario Kart 8", autor = "Nintendo", ano = 2014, codigo = "302", midia = Jogo, status = Disponivel}
+generico = Item {titulo = "", autor = "", ano = -50000, codigo = "", midia = Nenhum}
+x1 = Item {titulo = "Os Lusiadas", autor = "Camoes", ano = 1572, codigo = "1001", midia = Livro}
+x2 = Item {titulo = "Rimas", autor = "Camoes", ano = 1595, codigo = "1002", midia = Livro}
+x3 = Item {titulo = "Rimas", autor = "José Albano", ano = 1966, codigo = "103", midia = Livro}
+y1 = Item {titulo = "Festim Diabólico", autor = "Hitchcock", ano = 1948, codigo = "2001", midia = Filme}
+y2 = Item {titulo = "Sangue de Heróis", autor = "John Ford", ano = 1948, codigo = "2002", midia = Filme}
+y3 = Item {titulo = "Rastros de Ódio", autor = "John Ford", ano = 1956, codigo = "203", midia = Filme}
+z1 = Item {titulo = "Super Mario Bros.", autor = "Myamoto", ano = 1985, codigo = "3001", midia = Jogo}
+z2 = Item {titulo = "Mario Kart 8", autor = "Nintendo", ano = 2014, codigo = "3020", midia = Jogo}
 
-let lista = [x1,x2,x3,y1,y2,y3,z1,z2]
+lista = [x1,x2,x3,y1,y2,y3,z1,z2]
 
    
 w1 = Usuario {nome = "Joao", matricula = "12300", email = "test1@mail.com"}
